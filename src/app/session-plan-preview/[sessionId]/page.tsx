66908fdcc44db66056005322b5cba1e446e2ca71
mcjmccartney <mmccartney@cloud9assist.com>
2025-11-13 10:05:37 +0000

    Add better service worker state logging and reload logic

--- src/app/session-plan-preview/[sessionId]/page.tsx
+++ src/app/session-plan-preview/[sessionId]/page.tsx
@@ -356,23 +356,17 @@ export default function SessionPlanPreviewPage() {
   dangerouslySetInnerHTML={{
     __html: `
       @page {
-  size: A4;
-  margin: 180px 0 40px 0;   /* top matches header height */
-  background: #ecebdd;
+        size: A4;
+        margin: 100px 0 40px 0;
 
-  @top-center {
-    content: "";
-    background-image: url("https://i.ibb.co/Z1bz4gg9/Screenshot-2025-11-13-at-09-10-22.png");
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-position: center;
-    display: block;
-    height: 180px;   /* same as top margin */
-    width: 100%;
-  }
-}
+        @top-center {
+          content: "HEADER TEST";
+          color: red;
+          font-size: 40px;
+          font-weight: bold;
+        }
+      }
 
-      /* Required for paged.js to render margin boxes visually */
       .pagedjs_margin-content {
         display: block !important;
       }
