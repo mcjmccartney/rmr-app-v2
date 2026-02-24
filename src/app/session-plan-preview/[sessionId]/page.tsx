028dc09e8279463463427fb62d9a0486fbd9f850
mcjmccartney <mmccartney@cloud9assist.com>
2026-02-24 14:38:08 +0000

    Position Reminder above footer with no margin using absolute positioning

--- src/app/session-plan-preview/[sessionId]/page.tsx
+++ src/app/session-plan-preview/[sessionId]/page.tsx
@@ -283,6 +283,8 @@ function DynamicActionPointPages({ title, editableActionPoints, isPlaywrightMode
               {showReminderOnThisPage && (
                 <div
                   style={{
+                    position: 'absolute',
+                    bottom: '80px',
                     left: '3.4rem',
                     right: '3.4rem',
                     fontSize: '15px',
